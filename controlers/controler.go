46b0200dcdd41c0a87a7b4351392c20102685762
haimd <haimd@beetsoft.com.vn>
2020-06-17 16:39:34 +0700

    .

--- controlers/controler.go
+++ controlers/controler.go
@@ -31,11 +31,12 @@ func (ctl *Controller) AddDevice(context *gin.Context) {
 	fmt.Println(request)
 	data, err := ctl.deviceService.Add(request)
 	if err != nil {
-		utilitys.ResponseError400(context, err.Error())
 		fmt.Println("AddDevice 1")
+		utilitys.ResponseError400(context, err.Error())
+
 	} else {
-		utilitys.ResponseSuccess200(context, data, "success")
 		fmt.Println("AddDevice 2")
+		utilitys.ResponseSuccess200(context, data, "success")
 	}
 }
 func (ctl *Controller) DeleteDevice(context *gin.Context) {
