9c9a760a1a1ecba79d9921f6a0a1db6f3009b2c9
machai95 <haimd@beetsoft.com.vn>
2020-06-20 11:29:17 +0700

    Setup build test pc ubuntu

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type Config struct {
-	MySQLURL string `envconfig:"MYSQL_URL" default:"root:1@tcp(localhost:3306)/cm?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=True&loc=Local"`
+	MySQLURL string `envconfig:"MYSQL_URL" default:"root:bW90aGVyIGZ1Y2tlciBub29i@tcp(localhost:3306)/cm?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=True&loc=Local"`
 	APIKey   string `envconfig:"API_KEY" default:"controlnomey-hongminh-229297"`
 	SecretKet string `envconfig:"SECRET_KEY" default:"controlnomey-hongminh-229297"`
 }
