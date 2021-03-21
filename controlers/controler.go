23ecae99853f389698287813734ed0d4bc154789
haimd <haimd@beetsoft.com.vn>
2021-03-22 01:24:53 +0700

    Upload API

--- controlers/controler.go
+++ controlers/controler.go
@@ -131,9 +131,12 @@ func (ctl *Controller) Upload(context *gin.Context) {
 	var request dtos.UploadRequest
 	request.User_ID, _ = strconv.ParseInt(userID[0], 10, 64)
 	request.Mac = mac[0]
-	request.Files[0].Video_name = files[0].Filename
-	request.Files[0].Video_size = files[0].Size
-	request.Files[0].Video_time = 0
+	var f dtos.FileUpload
+
+	f.Video_name = files[0].Filename
+	f.Video_size = files[0].Size
+	f.Video_time = 0
+	request.Files = append(request.Files, f)
 
 	// err := context.ShouldBindJSON(&request)
 	// if err != nil {
