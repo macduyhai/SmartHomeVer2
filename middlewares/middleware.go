516cfe9ef15ba83a7f86bb30b7d24d2183fb79a0
haimd <haimd@beetsoft.com.vn>
2021-03-31 23:47:36 +0700

    Check loi CROS

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/macduyhai/SmartHomeVer2/utilitys"
@@ -11,20 +12,36 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "application/json")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(200)
-		} else {
-			c.Next()
+			c.AbortWithStatus(204)
+			return
 		}
+
+		c.Next()
 	}
 }
+
+// func CORSMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Writer.Header().Set("Content-Type", "application/json")
+// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+// 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(200)
+// 		} else {
+// 			c.Next()
+// 		}
+// 	}
+// }
 
 type CheckAPIKey struct {
 	ApiKey string
@@ -56,7 +73,7 @@ func RequestLogger() gin.HandlerFunc {
 		// rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
 		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf)) //We have to create a new Buffer, because rdr1 will be read.
 
-		// fmt.Println(readBody(rdr1)) // Print request body
+		log.Println(readBody(rdr2)) // Print request body
 
 		c.Request.Body = rdr2
 		c.Next()
