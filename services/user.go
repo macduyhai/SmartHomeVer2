f9c8a615276f33fd60b84961256afc852089a4bc
macduyhai <dh.duyhai@gmail.com>
2022-03-31 10:03:01 +0700

    .

--- services/user.go
+++ services/user.go
@@ -43,6 +43,7 @@ func (service *userServiceImpl) Login(request dtos.LoginRequest) (*dtos.LoginRes
 	array_byte := make([]byte, 8)
 	binary.LittleEndian.PutUint64(array_byte, uint64(user.ID))
 	// encode, err := RsaEncrypt([]byte(user.Username), config.PublicKey)
+	log.Println("config.Conf:")
 	log.Println(config.Conf)
 	encode, err := RsaEncrypt(array_byte, config.Conf.PublicKey) //service.config.PublicKey
 	if err != nil {
